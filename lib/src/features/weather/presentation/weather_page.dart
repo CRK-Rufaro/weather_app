3c2207e7a73aab4635021fc3e61b04b8ac12aff8
Rufaro Kembo <118901325+CRK-Rufaro@users.noreply.github.com>
2024-08-08 22:10:46 +0200

    Update weather_page.dart

--- lib/src/features/weather/presentation/weather_page.dart
+++ lib/src/features/weather/presentation/weather_page.dart
@@ -12,54 +12,56 @@ class WeatherPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-
-          //width: double.infinity,
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: AppColors.rainGradient,
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+      
+            //width: double.infinity,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: AppColors.rainGradient,
+              ),
             ),
-          ),
-          child: SafeArea(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Spacer(),
-                Container(
-                  alignment: Alignment.center,
-                  color: Colors.deepPurpleAccent,
-                  height: MediaQuery.of(context).size.height*0.4,
-                  width: MediaQuery.of(context).size.width,
-                  child: CurrentWeather()),
-                Spacer(),
-                Container(
-                  alignment: Alignment.center,
-                  color: Colors.brown,
-                  height: MediaQuery.of(context).size.height*0.25,
-                  child: ForecastWidget()),
-                Spacer(),
-                Container(
-                  padding: EdgeInsets.fromLTRB(30, 20, 30, 0),
-                  color: Colors.lime,
-                  alignment: Alignment.bottomRight,
-                  child: ConvertCelsiusFahrenheit()),
-                //Spacer(),
-                Container(
-                  color: Colors.amber,
-                  alignment: Alignment.topCenter,
-                  padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                  height: MediaQuery.of(context).size.height*0.2,
-                  child: CitySearchBox()),
-                Spacer(),
-                
-              ],
+            child: SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Spacer(),
+                  Container(
+                    alignment: Alignment.center,
+                    color: Colors.deepPurpleAccent,
+                    height: MediaQuery.of(context).size.height*0.4,
+                    width: MediaQuery.of(context).size.width,
+                    child: CurrentWeather()),
+                  Spacer(),
+                  Container(
+                    alignment: Alignment.center,
+                    color: Colors.brown,
+                    height: MediaQuery.of(context).size.height*0.25,
+                    child: ForecastWidget()),
+                  Spacer(),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(30, 20, 30, 0),
+                    color: Colors.lime,
+                    alignment: Alignment.bottomRight,
+                    child: ConvertCelsiusFahrenheit()),
+                  //Spacer(),
+                  Container(
+                    color: Colors.amber,
+                    alignment: Alignment.topCenter,
+                    padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                    height: MediaQuery.of(context).size.height*0.2,
+                    child: CitySearchBox()),
+                  Spacer(),
+                  
+                ],
+              ),
             ),
           ),
         ),
